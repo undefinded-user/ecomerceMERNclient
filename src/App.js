a608c1bec59423feffbac0e3848a931be1af145e
Undefined-user <undefinded-user@users.noreply.github.com>
2020-11-24 15:15:28 +0300

    Create simple Header component with ant design library
    -Add antd dependencie to the project

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Route, Switch } from 'react-router-dom'
 import Home from './pages/Home.js'
 import Login from './pages/auth/Login.js'
 import Register from './pages/auth/Register.js'
+import Header from './components/nav/Header'
 
  
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/login' component={Login}/>
-      <Route exact path='/register' component={Register}/>
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Switch>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css'
+
 ReactDOM.render(
   <React.StrictMode>
 	<BrowserRouter>
