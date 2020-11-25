61c7272e17b9baa23ac441978ca149b686e63160
Undefined-user <undefinded-user@users.noreply.github.com>
2020-11-25 09:54:17 +0300

    Create registration form
    -Use bootstrap materials
    -Use useState hook to control form

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+	const [email, setEmail] = useState('');
+
+	const handleSubmit = () => {
+
+	}
+
+	const handleChange = (e) => {
+		const { value } = e.target
+		setEmail(value)
+	}
+
+	const registerForm = () => {
+
+		return(		
+			<form onSubmit={handleSubmit}>
+				<input 
+					type='email'
+					className='form-control' 
+					id='email'
+					value={email}
+					onChange={handleChange}
+					placeholder='Email'
+					autoFocus
+				/>
+			</form>
+		)  	
+	}
+
 	return(
-		<div>
-			<p>Register page</p>
+		<div className="container p-5">
+			<div className='row'>
+				<div className='col-md-6 offset-md-3'>
+					<h1>Register</h1>
+					{registerForm()}
+					<button type='sybmit' className="btn btn-raised">
+						Register{email? ' / ' + email : ''}
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
