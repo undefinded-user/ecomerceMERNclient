2ca3fb489e2869f527c186aa35d9b8fb21bdcba3
Undefined-user <undefinded-user@users.noreply.github.com>
2020-11-26 21:40:55 +0300

    Change initial user state value to null

--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -1,4 +1,4 @@
-export const userReducer = (state={}, action) =>{
+export const userReducer = (state=null, action) =>{
 	switch(action.type) {
 		case 'LOGGED_IN_USER':{
 			return {...state, ...action.payload}
