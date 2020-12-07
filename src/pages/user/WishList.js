b98087f6f10690c4df781ba625b2abf36925ac2f
Undefined-user <undefinded-user@users.noreply.github.com>
2020-12-07 18:48:43 +0300

    Example pages of  protected user`s routes

--- src/pages/user/WishList.js
+++ src/pages/user/WishList.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import UserNav from '../../components/nav/UserNav'
 
-const History = () => {
+const WishList = () => {
 	return (
 		<div className='container-fluid'>
 			<div className='row'>
 				<div className='colmd-2'><UserNav /></div>
-				<div className='col'>User history</div>
+				<div className='col'>Wish list page</div>
 			</div>
 		</div>
 	)
 }
 
-export default History
+export default WishList
