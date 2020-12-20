56fbc0f8290b4b48519c7bca422a9b3b05a1638e
Undefined-user <undefinded-user@users.noreply.github.com>
2020-12-20 23:58:48 +0300

    CreateCategory page
    -Connect to backend to for cteatind categories

---
+++ src/pages/admin/category/CategoryCreate.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react'
+import {useSelector} from 'react-redux'
+import AdminNav from '../../../components/nav/AdminNav'
+import CategoryItem from '../../../components/category/CategoryItem'
+import CategoryForm from '../../../components/forms/CategoryForm'
+import LocalSearch from '../../../components/forms/LocalSearch'
+
+import {toast} from 'react-toastify'
+
+import {createCategory, getCategories} from '../../../functions/category.js'
+
+
+const CategoryCreate = () => {
+	const [name, setName] = useState('')
+	const [keyWord, setKeyWord] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
+	const [categories, setCategories] = useState([])
+	const user = useSelector((state) => state.user)
+
+	useEffect(()=> {
+		loadCategories()
+	}, [])
+
+	const loadCategories = async() => {
+		const categories = await getCategories()
+		setCategories(categories.data)
+	}
+
+	const handleSubmit = async (e) => {
+		try{
+			e.preventDefault()
+			setIsLoading(true)
+			const res = await createCategory(user.token, name)
+			toast.success(`"${res.data.name}" is created`)
+			setIsLoading(false)
+			setName('')
+			loadCategories()
+		} catch(error){
+			setIsLoading(false)
+			toast.error(error.response.data)
+		}
+		
+	}	
+
+	const searched = (keyWord) => (category) => category.name.toLowerCase().includes(keyWord)
+	return (
+		<div className='container-fluid'>
+			<div className='row'>
+				<div className='col-md-2'>
+					<AdminNav />
+				</div>
+				<div className='col'>
+					{isLoading? <h4>Loading...</h4> : <h4>Create new category</h4>}
+					<CategoryForm handleSubmit={handleSubmit} name={name} setName={setName} btnText='Create' />
+					<LocalSearch kewWord={keyWord} setKeyWord={setKeyWord} />
+					<hr />
+					{categories.filter(searched(keyWord)).map((category) => <CategoryItem  setCategories={setCategories} category={category} key={category._id}/>)}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default CategoryCreate
