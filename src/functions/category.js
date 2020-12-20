26b3fd0a810d54a9ec181951192c415c555193db
Undefined-user <undefinded-user@users.noreply.github.com>
2020-12-20 23:54:59 +0300

    Category functions
    -makes request to backend api

---
+++ src/functions/category.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+export const getCategories = async () => {
+	return await axios.get(`${process.env.REACT_APP_API}/categories`)
+}
+
+export const getCategory = async (slug) => {
+	return await axios.get(`${process.env.REACT_APP_API}/category/${slug}`)
+}
+
+export const createCategory = async (authtoken, category) => {
+	return await axios.post(`${process.env.REACT_APP_API}/category`, {name : category}, {
+		headers: {
+			authtoken
+		}
+	})
+}
+
+export const updateCategory = async (slug, authtoken, category) => {
+	return await axios.put(`${process.env.REACT_APP_API}/category/${slug}`, {name:category}, {
+		headers: {
+			authtoken
+		}
+	})
+}
+
+export const removeCategory = async (slug, authtoken) => {
+	return await axios.delete(`${process.env.REACT_APP_API}/category/${slug}`, {
+		headers: {
+			authtoken
+		}
+	})
+}
