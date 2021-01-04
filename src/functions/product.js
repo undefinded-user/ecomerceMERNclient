c4de6b110143d1c2f505293162ba633540086564
Undefined-user <undefinded-user@users.noreply.github.com>
2021-01-04 19:31:00 +0300

    Product help functions
    Each funtion makes async req to server endpoint

---
+++ src/functions/product.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+export const getProducts = async (count) => {
+	return await axios.get(`${process.env.REACT_APP_API}/products/${count}`)
+}
+
+export const getProduct = async (slug) => {
+	return await axios.get(`${process.env.REACT_APP_API}/product/${slug}`)
+}
+
+export const createProduct = async (authtoken, info) => {
+	return await axios.post(`${process.env.REACT_APP_API}/product`, info, {
+		headers: {
+			authtoken
+		}
+	})
+}
+
+export const updateProduct = async (slug, authtoken, product) => {
+	return await axios.put(`${process.env.REACT_APP_API}/product/${slug}`, product, {
+		headers: {
+			authtoken
+		}
+	})
+}
+
+export const removeProduct = async (slug, authtoken) => {
+	return await axios.delete(`${process.env.REACT_APP_API}/product/${slug}`, {
+		headers: {
+			authtoken
+		}
+	})
+}
