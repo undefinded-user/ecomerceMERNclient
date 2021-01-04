8a5de31257a9a6ef0735c5054d7da9282f34fa74
Undefined-user <undefinded-user@users.noreply.github.com>
2021-01-04 19:39:35 +0300

    Update index.js
    Add link styles from bootstrap material UI

--- src/components/nav/UserNav/index.js
+++ src/components/nav/UserNav/index.js
@@ -6,15 +6,15 @@ const UserNav = () => {
 		<nav>
 			<ul className='nav flex-column'>
 				<li className='nav-item'>
-					<Link to='/user/history'>History</Link>
+					<Link to='/user/history' className='nav-link'>History</Link>
 				</li>
 
 				<li className='nav-item'>
-					<Link to='/user/password'>Change password</Link>
+					<Link to='/user/password' className='nav-link'>Change password</Link>
 				</li>
 
 				<li className='nav-item'>
-					<Link to='/user/wishlist'>Wish list</Link>
+					<Link to='/user/wishlist' className='nav-link'>Wish list</Link>
 				</li>
 			</ul>
 		</nav>
