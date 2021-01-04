d67186532ce21daeac83a1b2901b5486511aab4a
Undefined-user <undefinded-user@users.noreply.github.com>
2021-01-04 21:29:06 +0300

    Admin Dashboard layout

--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import AdminNav from '../../components/nav/AdminNav'
 
 const AdminDashboard = () => {
 	return (
-		<h1>Main admin page</h1>
+		<div className='container-fluid'>
+			<div className='row'>
+				<div className='col-md-2'>
+					<AdminNav />
+				</div>
+				<div className='col'>
+					<h4>Adnin Dashboard</h4>
+				</div>
+			</div>
+		</div>
 	)
 }
 
