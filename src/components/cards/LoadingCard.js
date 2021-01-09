481bfb033b310a71ec450e0400f5a51f8aaa8995
Undefined-user <undefinded-user@users.noreply.github.com>
2021-01-09 21:07:24 +0300

    Create LoadingCard.js
    Take count prop
    Based on the count display several empty(loading) cards

---
+++ src/components/cards/LoadingCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import {Card, Skeleton} from 'antd'
+
+const LoadingCard = ({count}) => {
+	const generateContent = () => {
+		const content = []
+		for(let i = 0; i<count; i++){
+			content.push(
+				<div className='col-md-4 p-1'>
+					<Card>
+						<Skeleton active></Skeleton>
+					</Card >
+				</div>
+			)
+		}
+		return content
+	}
+
+	
+
+	return (
+		<>
+			{generateContent()}
+		</>		
+	)
+}
+
+export default LoadingCard
