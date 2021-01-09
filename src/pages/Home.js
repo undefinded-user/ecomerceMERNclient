c4c8e3f15f074e669e4927e2ec96491f6b4a75ba
Undefined-user <undefinded-user@users.noreply.github.com>
2021-01-09 21:06:08 +0300

    Simple layout to home page
    Fetching products information
    Map through products to display single product
    Conditional rendering based on loading status

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+import ProductCard from '../components/cards/ProductCard'
+import LoadingCard from '../components/cards/LoadingCard'
+
+import {getProducts} from '../functions/product'
+
+import {toast} from 'react-toastify'
 
 const Home = () => {
+	const [products, setProducts] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
+
+	useEffect(()=>{
+		loadProducts()
+	}, [])
+
+	const loadProducts = async () => {
+		try{
+			setIsLoading(true)
+			const res = await getProducts(3)
+			setProducts(res.data)
+			setIsLoading(false)
+		} catch(error){
+			setIsLoading(false)
+			console.log(error.response.data)
+			toast.error('Load products failed')
+		}		
+	}
+
 	return (
-		<div>
-			<p>Home page</p>
-		</div>
+		<>
+			<div className='container'>
+				<div className='row'>
+					{!isLoading&&products.map((product) => (
+						<div key={product._id} className='col-md-4'>
+							<ProductCard product={product} />
+						</div>
+					))}
+					{isLoading&&<LoadingCard count={3} />}
+				</div>
+			</div>
+		</>
 	)
 }
 
