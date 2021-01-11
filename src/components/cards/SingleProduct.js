69f4fa3ea5435cbd6499ce0e617a98ca5afbff3a
Undefined-user <undefinded-user@users.noreply.github.com>
2021-01-11 19:32:23 +0300

    SingleProduct component
    Display
      *Image corousel from antd
      *ProductItemsList
      *Tabs with description and additional information

---
+++ src/components/cards/SingleProduct.js
@@ -0,0 +1,68 @@
+import React, {useEffect} from 'react'
+
+import {Link} from 'react-router-dom'
+
+import ProductItemsList from './ProductItemsList'
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import {Carousel} from 'react-responsive-carousel'
+
+import DefaultImage from '../../images/4-devices-2.png'
+
+import {HeartOutlined, ShoppingCartOutlined} from '@ant-design/icons'
+import {Card, Tabs} from 'antd'
+const {TabPane} = Tabs
+
+const SingleProduct = ({product}) => {
+	const {title,images, description} = product
+
+	return (
+		<>
+			<div className='col-md-7'>
+				{images&&images.length? 
+					(<Carousel showArrows={true} autoPlay infiniteLoop>
+						{images.map((image)=><img src={image.url} key={image.public_id} />)}
+					</Carousel>)
+					:
+					(<Card
+						cover={
+					      <img
+					      	className='p-1 card-image'
+					        src={DefaultImage}
+					   		/>
+						}
+					></Card>)
+				}
+				<Tabs className='mt-2' type='card'>
+					<TabPane className='pl-3 pr-3' tab='Description' key='1'>
+						{description&&description}
+					</TabPane>
+					<TabPane className='pl-3 pr-3' tab='More' id='2'>
+						Some additional information
+					</TabPane>
+				</Tabs>
+			</div>
+			<div className='col-md-5'>
+				<h1 className='bg-info p-3 text-center'>{title}</h1>
+				<Card
+					actions={[
+						<>
+							<ShoppingCartOutlined className='text-success' />
+							<br/>
+							Add to Cart
+						</>,
+						<Link to=''>
+							<HeartOutlined  className='text-info' />
+							<br/>
+							Add to Wishist
+						</Link>
+					]}
+				>
+					<ProductItemsList product={product} />
+				</Card>
+			</div>
+		</>
+	)
+}
+
+export default SingleProduct 
