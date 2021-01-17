facca37f183a5f117ec9a7aa0ee985322ac15ed8
Undefined-user <undefinded-user@users.noreply.github.com>
2021-01-17 17:39:46 +0300

    Add leave rating feature

--- src/components/cards/SingleProduct.js
+++ src/components/cards/SingleProduct.js
@@ -3,6 +3,9 @@ import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom'
 
 import ProductItemsList from './ProductItemsList'
+import RatingModal from '../modals/RatingModal'
+
+import StarRating from 'react-star-ratings'
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import {Carousel} from 'react-responsive-carousel'
@@ -14,7 +17,7 @@ import {Card, Tabs} from 'antd'
 const {TabPane} = Tabs
 
 const SingleProduct = ({product}) => {
-	const {title,images, description} = product
+	const {title,images, description, _id} = product
 
 	return (
 		<>
@@ -43,7 +46,10 @@ const SingleProduct = ({product}) => {
 				</Tabs>
 			</div>
 			<div className='col-md-5'>
+				
+
 				<h1 className='bg-info p-3 text-center'>{title}</h1>
+
 				<Card
 					actions={[
 						<>
@@ -55,7 +61,17 @@ const SingleProduct = ({product}) => {
 							<HeartOutlined  className='text-info' />
 							<br/>
 							Add to Wishist
-						</Link>
+						</Link>,
+						<RatingModal>	
+							<StarRating
+								name={_id}
+								numberOfStars={5}
+								rating={3}
+								changeRating={(newRating, name) => console.log('RATING', newRating, 'NAME', name)}
+								isSelectable={false}
+								starRatedColor='red'
+						 	/>
+						</RatingModal>
 					]}
 				>
 					<ProductItemsList product={product} />
